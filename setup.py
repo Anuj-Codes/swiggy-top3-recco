b75361e1255e096d24a8e53cba8ca127a1b02041
Anuj-Codes <anuj.shrivastav24@gmail.com>
2023-11-28 00:31:52 +0530

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This will help to reccomend top food item from swiggy home page',
+    author='Anuj Shrivastav',
+    license='',
+)
